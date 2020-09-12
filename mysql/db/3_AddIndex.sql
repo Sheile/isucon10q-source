e6c76a831a62e0f062ae22e4ef432adc349fea93
Sheile <sheile1024@gmail.com>
2020-09-12 15:29:23 +0900

    Add index on latitude/longitude

--- mysql/db/3_AddIndex.sql
+++ mysql/db/3_AddIndex.sql
@@ -1,5 +1,6 @@
 ALTER TABLE isuumo.estate ADD INDEX index_estate_rent(rent);
 ALTER TABLE isuumo.estate ADD INDEX index_estate_popularity(popularity);
+ALTER TABLE isuumo.estate ADD INDEX index_estate_latitude_longitude(latitude, longitude);
 
 ALTER TABLE isuumo.chair ADD INDEX index_chair_stock(stock);
 ALTER TABLE isuumo.chair ADD INDEX index_chair_price(price);
